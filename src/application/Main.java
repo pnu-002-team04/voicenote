b87142d03fc890acb326ec3df8be58ab002200b2
kukunana <kudange@naver.com>
2019-04-05 18:35:23 +0900

    슬라이드 타이틀 디자인
    #VOEA-74

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,9 @@ import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	@Override
+	
+	
+	
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = (Parent)FXMLLoader.load(getClass().getResource("Sample.fxml"));
@@ -16,6 +19,11 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+		    
+		        primaryStage.setTitle("VoiceNote");
+		        primaryStage.setScene(scene);
+		        primaryStage.show();
 		} catch(Exception e) {
 		}
 	}
